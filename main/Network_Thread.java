e47fcd5516cc962bb79c47b3d1300e231d986127
Juventer <juventer@web.de>
2021-11-27 19:38:53 +0100

    Methodes created
    Network_Thread() created
    run() created
    close() created

--- main/Network_Thread.java
+++ main/Network_Thread.java
@@ -1,3 +1,33 @@
 package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Network_Thread extends Thread{
+	public Network_Thread(Controller pController) {
+		this.start();
+	}
+	public void run() {
+		
+		while(!stop) {
+			
+			
+			
+			
+			
+		}
+		
+	}
+	
+	
+	public void close() {
+		
+		stop = true;
+		
+		
+		
+	}
 }
